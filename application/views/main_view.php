2457e002298050b966266f61bb4628519a951284
Alexander Burton <abrx21@gmail.com>
2015-06-29 19:17:52 -0400

    Added the Glyphicons

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,25 +1,5 @@
 <?php include('header.php'); ?>
 <?php include('nav.php'); ?>
-		<div id="sidebar" style="width:210px;">
-		  <ul class="sidebar-nav">
-		    <li class="sidebar-brand">
-		      <a href="#">Formulex</a>
-		    </li>
-		    <li><a href="#">Dashboard</a></li>
-		    <li><a href="#">Shortcuts</a></li>
-		    <li><a href="#">Overview</a></li>
-		    <li><a href="#">About</a></li>
-		    <li><a href="#">Service</a></li>
-		    <li><a href="#">Contact</a></li>
-		    <li><a href="#">Link</a></li>
-		    <li><a href="#">Link</a></li>
-		  </ul>
-		</div>
-    <div id="content">
-        
-        <h1>Welcome to Formulex</h1>
-        <p>To get started please read the documentation at <a href="http://pip.dev7studios.com/">http://pip.dev7studios.com</a>.</p>
-        
-    </div>
-
+<?php include('sidebar.php'); ?>
+<?php include('home.php'); ?>
 <?php include('footer.php'); ?>
---
+++ application/views/sidebar.php
@@ -0,0 +1,15 @@
+<div id="sidebar" style="width:210px;">
+  <ul class="sidebar-nav">
+    <li class="sidebar-brand">
+      <a href="#">Formulex
+    </li>
+    <li><a href="localhost/formulex">Home <span class="glyphicon glyphicon-home"></span></a></li>
+    <li><a href="#">New Formula <span class="glyphicon glyphicon-plus"></span></a></li>
+    <li><a href="#">Formula Log <span class="glyphicon glyphicon-list"></span></a></li>
+    <li><a href="#">Databases <span class="glyphicon glyphicon-list-alt"></span></a></li>
+    <li><a href="#">Dashboard <span class="glyphicon glyphicon-th-large"></span></a></li>
+    <li><a href="#">Messages <span class="glyphicon glyphicon-inbox"></span></a></li>
+    <li><a href="#">Link <span class="glyphicon glyphicon-"></span></a></li>
+    <li><a href="#">Link <span class="glyphicon glyphicon-"></span></a></li>
+  </ul>
+</div>
